ed9928959e16cbbdd67b582f698f2bd9e0ffc059
bhandekunal16 <bhandekunal16@gmail.com>
2025-03-19 16:16:06 +0530

    `Refactored code in app.js with added try-catch block and minor formatting changes`

--- app.js
+++ app.js
@@ -80,39 +80,43 @@ app.get("/", (req, res) => {
 });
 
 app.post("/upload", upload.single("file"), async (req, res) => {
-  if (req.file) {
-    if (req.file.filename.endsWith(".csv")) {
-      const ele = await new csv().read(`uploads/${req.file.filename}`);
-      const report = new csv().generate_report(ele);
-      const download = await new csv().write(
-        `report/${new generator().getFormattedDate()}_IER.csv`,
-        {
-          totalExpanse: report.totalExpanse,
-          totalIncome: report.totalIncome,
-          bigExpanse: report.bigExpanse,
-          bigIncome: report.bigIncome,
-          bigExpanseName: report.bigExpanseName,
-          bigIncomeName: report.bigIncomeName,
-          totalTransactions: report.totalTransactions,
-          avgIncome: report.avgIncome,
-          avgExpense: report.avgExpense,
-        }
-      );
-      await Promise.all([
-        new csv().append(`report/ED.csv`, report.expanse),
-        new csv().append(`report/ID.csv`, report.income),
-        new csv().maintain_account(`report/account.csv`, {
-          totalExpanse: report.totalExpanse,
-          totalIncome: report.totalIncome,
-          totalTransactions: report.totalTransactions,
-        }),
-      ]);
-      res.send(`<a href="${download}" style="padding: 10px 15px; background-color: #007bff; color: white; text-decoration: none; border-radius: 5px; display: inline-block;">
-                  Download File ${download}</a>
-`);
+  try {
+    if (req.file) {
+      if (req.file.filename.endsWith(".csv")) {
+        const ele = await new csv().read(`uploads/${req.file.filename}`);
+        const report = new csv().generate_report(ele);
+        const download = await new csv().write(
+          `report/${new generator().getFormattedDate()}_IER.csv`,
+          {
+            totalExpanse: report.totalExpanse,
+            totalIncome: report.totalIncome,
+            bigExpanse: report.bigExpanse,
+            bigIncome: report.bigIncome,
+            bigExpanseName: report.bigExpanseName,
+            bigIncomeName: report.bigIncomeName,
+            totalTransactions: report.totalTransactions,
+            avgIncome: report.avgIncome,
+            avgExpense: report.avgExpense,
+          }
+        );
+        await Promise.all([
+          new csv().append(`report/ED.csv`, report.expanse),
+          new csv().append(`report/ID.csv`, report.income),
+          new csv().maintain_account(`report/account.csv`, {
+            totalExpanse: report.totalExpanse,
+            totalIncome: report.totalIncome,
+            totalTransactions: report.totalTransactions,
+          }),
+        ]);
+        res.send(`<a href="${download}" style="padding: 10px 15px; background-color: #007bff; color: white; text-decoration: none; border-radius: 5px; display: inline-block;">
+                    Download File ${download}</a>
+  `);
+      }
+    } else {
+      res.send("No file uploaded");
     }
-  } else {
-    res.send("No file uploaded");
+  } catch (error) {
+    res.send("wrong file input please insert correct data");
   }
 });
 
